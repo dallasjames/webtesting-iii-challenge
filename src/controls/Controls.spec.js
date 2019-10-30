8903d86d9e5afd673f6b7f54feebde9b81b0b061
Dallas James <dallasjames42@gmail.com>
2019-10-30 17:46:20 -0600

    dashboard passes

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,5 @@
-// Test away!
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Controls from './Controls';
+
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
-// Test away
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+
+afterEach(rtl.cleanup);
+
+test('Dashboard shows controls and display', () => {
+    const wrapper = rtl.render(<Dashboard />)
+    
+    const element = wrapper.getByText(/unlocked/i)
+
+    const element2 = wrapper.getByText(/open/i)
+
+    expect(element).toBeVisible()
+    expect(element2).toBeVisible()
+});
