849d4c8a85aa60782d14430fd04089babcf31363
Dallas James <dallasjames42@gmail.com>
2019-10-30 17:38:16 -0600

    display test passed

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,17 @@
-// Test away!
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Display from './Display';
+
+afterEach(rtl.cleanup);
+
+test('displays', () => {
+    const wrapper = rtl.render(<Display />)
+    
+    const element = wrapper.getByTestId(/lock/i)
+
+    const element2 = wrapper.getByTestId(/close/i)
+
+    expect(element).toBeVisible()
+    expect(element2).toBeVisible()
+});
